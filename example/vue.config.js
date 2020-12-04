acdee0f344db7afef5ff32cb6829af204b6bfba8
Ramil Gallyamov <gallyamov.ramil@tatar.ru>
2020-12-04 17:42:19 +0300

    set correct publicPath for demo

--- example/vue.config.js
+++ example/vue.config.js
@@ -1,4 +1,6 @@
-// @see https://github.com/vuejs/vue-cli/issues/2948
 module.exports = {
-  chainWebpack: config => config.resolve.symlinks(false)
+  // @see https://github.com/vuejs/vue-cli/issues/2948
+  chainWebpack: config => config.resolve.symlinks(false),
+  // @see https://cli.vuejs.org/ru/config/#publicpath
+  publicPath: process.env.NODE_ENV === 'production' ? '/webgl-helix-ribbon/example/dist/' : '/'
 }
